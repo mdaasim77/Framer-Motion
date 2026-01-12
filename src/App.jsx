c4c71ee3784021624d840851dce4641ed3c0f0eb
mdaasim77 <khanaasim9910@gmail.com>
2026-01-12 12:24:36 +0530

    Add new file with same work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { motion, transform } from "motion/react";
+import First from "./Components/A.one";
 
 export default function App() {
   return (
     <>
-      <motion.div
-        className="box"
-        animate={{ x: 1000, y: 100, rotate: 360, scale: 0.5 }}
-        transition={{ duration: 3, delay: 1 }}
-      ></motion.div>
+      <First />
     </>
   );
 }
