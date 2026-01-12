e8f0d2869e9b2a2853bcdbbcbff067036a1a5bdd
mdaasim77 <khanaasim9910@gmail.com>
2026-01-12 14:33:03 +0530

    add a square also for test

--- src/Components/Second.jsx
+++ src/Components/Second.jsx
@@ -14,6 +14,16 @@ export default function Second() {
         src="src\Image\image.avif"
         alt="image"
       />
+      <motion.div
+        className="square"
+        initial={{ x: 0, y: 0 }}
+        animate={{
+          x: [0, 800, 800, 0, 0],
+          y: [0, 0, 300, 300, 0],
+          rotate: [360, 0, 360, -360, 0],
+        }}
+        transition={{ duration: 8, ease: "anticipate" }}
+      ></motion.div>
     </>
   );
 }
