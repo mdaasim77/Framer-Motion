04d68f3888571de619a44b4e01ee59f4027307c0
mdaasim77 <khanaasim9910@gmail.com>
2026-01-12 12:15:34 +0530

    first transitation done move x,y,rotate,duration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { motion, transform } from "motion/react";
 
 export default function App() {
-  return <div>Framer Motion</div>;
+  return (
+    <>
+      <motion.div
+        className="box"
+        animate={{ x: 1000, y: 100, rotate: 360, scale: 0.5 }}
+        transition={{ duration: 3, delay: 1 }}
+      ></motion.div>
+    </>
+  );
 }
