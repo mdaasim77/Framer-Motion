63d2a62c46062c0dd064540f1c4978c52bcb9cea
mdaasim77 <khanaasim9910@gmail.com>
2026-01-16 13:27:32 +0530

    add scale in box when scroll box size reduce to 0.6

--- src/Components/FigmaProject.jsx
+++ src/Components/FigmaProject.jsx
@@ -8,6 +8,7 @@ export default function FigmaProject() {
   const rotate2 = useTransform(scrollYProgress, [0, 1], [0, -30]);
   const rotate3 = useTransform(scrollYProgress, [0, 1], [0, -20]);
   const rotate4 = useTransform(scrollYProgress, [0, 1], [0, -10]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.6]);
 
   return (
     <div className="bg-gray-800 min-h-[260vh] w-full relative overflow-hidden ">
@@ -22,23 +23,23 @@ export default function FigmaProject() {
         className="flex gap-5 sticky top-[60vh] right-0 p-10 translate-x-3/5 translate-y-1/2"
       >
         <motion.div
-          style={{ rotate: rotate1 }}
+          style={{ rotate: rotate1, scale }}
           initial={{ scale: 1 }}
           animate={{ scale: 0.7 }}
           className="bg-gray-400 h-56 w-56 rounded-2xl"
         ></motion.div>
         <motion.div
-          style={{ rotate: rotate2 }}
+          style={{ rotate: rotate2, scale }}
           animate={{ scale: 0.7 }}
           className="bg-green-600 h-56 w-56 rounded-2xl"
         ></motion.div>
         <motion.div
-          style={{ rotate: rotate3 }}
+          style={{ rotate: rotate3, scale }}
           animate={{ scale: 0.7 }}
           className="bg-blue-400 h-56 w-56 rounded-2xl"
         ></motion.div>
         <motion.div
-          style={{ rotate: rotate4 }}
+          style={{ rotate: rotate4, scale }}
           animate={{ scale: 0.7 }}
           className="bg-pink-400 h-56 w-56 rounded-2xl"
         ></motion.div>
