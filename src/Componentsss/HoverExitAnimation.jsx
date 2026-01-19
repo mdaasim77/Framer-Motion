08981bcf1264b8fe01836d3110b697286c4c5f3d
mdaasim77 <khanaasim9910@gmail.com>
2026-01-19 15:08:49 +0530

    add logo and card

--- src/Componentsss/HoverExitAnimation.jsx
+++ src/Componentsss/HoverExitAnimation.jsx
@@ -3,8 +3,24 @@ import { motion } from "motion/react";
 
 export default function HoverExitAnimation() {
   return (
-    <div className="h-screen w-full flex items-center justify-center bg-gray-600">
-      <div className="bg-white h-96 w-56">card</div>
+    <div className="h-screen w-full flex items-center justify-center">
+      <div className="bg-white h-96 w-72 shadow-2xl rounded-2xl ">
+        <h2 className="text-[14px] font-bold text-start ms-4  mt-4 pt-1 text-gray-500">
+          Lorem, ipsum dolor.
+        </h2>
+        <p className="text-start text-[14px] ms-4  mt-4  pt-1 text-gray-500">
+          Lorem ipsum vitae illo doloremque sint facilis officia?
+        </p>
+        <div className="flex items-center justify-center shadow-2xl">
+          <button className="  border py-2 text-[12px] px-3 flex rounded-md mt-2 text-center">
+            <img
+              src="/src/Assets/Logo.png"
+              className="w-5 bg-amber-500 me-1.5"
+            />
+            Aceternity
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
