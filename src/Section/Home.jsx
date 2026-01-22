746914cfd8c34e91c729c0cabf4268c7bb56b469
mdaasim77 <khanaasim9910@gmail.com>
2026-01-22 11:16:02 +0530

    website black section working fine

--- src/Section/Home.jsx
+++ src/Section/Home.jsx
@@ -70,7 +70,7 @@ const Home = () => {
     <>
       <div className="w-full min-h-[200vh] relative bg-black">
         {/* text section */}
-        <div className="pt-20 px-20  text-white ">
+        <div className="pt-20 px-20  text-white sticky top-4">
           <h2 className="text-orange-400 font-bold">Explore Service</h2>
           <h2 className="font-bold text-2xl">
             WE HAVE DONE <br /> COUNTLESS WORK!
